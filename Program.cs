21ecc4d8fa949ef794cc23feb907b36bd036cb04
Phong Vo <vhphong>
2020-06-15 22:25:56 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Date created: 6/15/2020
+// Time: 2 hrs
+// Description: Written in C# and MySQL to help the user manage customer 's information 
+// Note: Add-in MYSQL for Visual Studio is needed
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomerInformation
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new frmCustInfo());
+		}
+	}
+}
